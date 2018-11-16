235aac92fb6d26565c76687cb221023cce010239
imxlss <imxlss@163.com>
2018-11-16 17:58:04 +0800

    create api done

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const CONFIG = {
     key: 'bllo',
     maxAge: 1000 * 60 * 5, // 有效期
     overwrite: true,
-    signed: true,
+    signed: true
 };
 
 /* -----------------model------------------ */
@@ -78,7 +78,6 @@ const ArticleSchema = new Schema({
 const UserModel = mongoose.model('User', UserSchema);
 const ArticleModel = mongoose.model('Article', ArticleSchema);
 
-
 /* -----------------router------------------ */
 // 注册
 const register = async ctx => {
@@ -93,6 +92,7 @@ const register = async ctx => {
     );
     if (!username || !password) {
         console.log('用户名或密码不存在！');
+        // return ctx.error({ msg: 'errorerror' });
         return (ctx.body = {
             msg: 'error'
         });
@@ -189,21 +189,61 @@ const signout = async ctx => {
 const createArticle = async ctx => {
     const user = ctx.session.user;
 
-    if (!user) return (ctx.body = {
-        status: 'fail',
-        msg: '请先进行登陆'
-    });
+    if (!user)
+        return (ctx.body = {
+            status: 'fail',
+            msg: '请先进行登陆'
+        });
 
     const {
-        name,
         _id
     } = user;
     const data = JSON.parse(ctx.request.body);
+    console.log(data);
+    if (!data)
+        return (ctx.body = {
+            status: 'fail',
+            msg: '数据发送失败'
+        });
 
-    if (!data) return (ctx.body = {
-        status: 'fail',
-        msg: '数据发送失败'
+    const isExist = await ArticleModel.findOne({
+        title: data.title
     });
+    if (isExist)
+        return (ctx.body = {
+            status: 'fail',
+            msg: '文章标题重复'
+        });
+    data.author = _id;
+    const result = await ArticleModel.create(data);
+    if (!result) return ctx.body = {
+        status: 'fail',
+        msg: '文章创建失败'
+    };
+
+    return ctx.body = {
+        status: 'success',
+        msg: '文章创建成功',
+        data: data
+    }
+};
+
+const getArticleDetail = async ctx => {
+    const {
+        id
+    } = ctx.query;
+
+    const data = await ArticleModel
+        .findById(id)
+        .populate('author', {
+            password: 0
+        });
+    if (!data) return ctx.body = {
+        status: 'fail',
+        msg: '获取失败'
+    }
+
+    const review = data.review + 1;
 }
 
 /* -----------------一些中间件------------------ */
@@ -213,8 +253,8 @@ app.use(koaBody());
 app.use(bodyParser());
 
 // cors 解决跨域
+// ctx.set('Access-Control-Allow-Origin', 'http://www.xlss.link');
 router.all('/api/*', async (ctx, next) => {
-    // ctx.set('Access-Control-Allow-Origin', 'http://www.xlss.link');
     ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000');
     ctx.set('Access-Control-Allow-Credentials', 'true');
     await next();
@@ -223,7 +263,9 @@ router.all('/api/*', async (ctx, next) => {
 router
     .post('/api/register', register)
     .post('/api/signin', signin)
-    .get('/api/signout', signout);
+    .get('/api/signout', signout)
+
+    .post('/api/create', createArticle)
 
 app.use(router.routes()).use(router.allowedMethods());
 
