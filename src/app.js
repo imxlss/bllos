eafb443901e69a50abe5776e15811d1055f9efe1
imxlss <imxlss@163.com>
2018-11-19 18:29:13 +0800

    add getArticle api

--- src/app.js
+++ src/app.js
@@ -12,21 +12,22 @@ const Schema = mongoose.Schema;
 
 app.keys = ['bllo:secret'];
 const CONFIG = {
-    key: 'bllo',
-    maxAge: 1000 * 60 * 5, // 有效期
-    overwrite: true,
-    signed: true
+  key: 'bllo',
+  maxAge: 1000 * 60 * 5, // 有效期
+  overwrite: true,
+  signed: true
 };
 
 /* -----------------model------------------ */
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     }
     /*     createdAt: {
             type: Date,
@@ -36,44 +37,51 @@ const UserSchema = new Schema({
             type: Date,
             default: Date.now
         } */
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
-const ArticleSchema = new Schema({
+const ArticleSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: 'User'
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     text_link: {
-        type: String
+      type: String
     },
-    tag: [{
+    tag: [
+      {
         type: String
-    }],
+      }
+    ],
     summary: {
-        type: String
+      type: String
     },
     // comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
     review: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     isOriginal: {
-        type: Boolean,
-        required: true
+      type: Boolean,
+      required: true
     }
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 const UserModel = mongoose.model('User', UserSchema);
 const ArticleModel = mongoose.model('Article', ArticleSchema);
@@ -81,170 +89,186 @@ const ArticleModel = mongoose.model('Article', ArticleSchema);
 /* -----------------router------------------ */
 // 注册
 const register = async ctx => {
-    // ctx.request.body 是一个json字符串
-    const {
-        username,
-        password,
-        createdAt,
-        updateAt
-    } = JSON.parse(
-        ctx.request.body
-    );
-    if (!username || !password) {
-        console.log('用户名或密码不存在！');
-        // return ctx.error({ msg: 'errorerror' });
-        return (ctx.body = {
-            msg: 'error'
-        });
-    }
-    const isExist = await UserModel.findOne({
-        username
+  // ctx.request.body 是一个json字符串
+  const { username, password, createdAt, updateAt } = JSON.parse(
+    ctx.request.body
+  );
+  if (!username || !password) {
+    console.log('用户名或密码不存在！');
+    // return ctx.error({ msg: 'errorerror' });
+    return (ctx.body = {
+      msg: 'error'
     });
-    if (isExist) {
-        return (ctx.body = {
-            status: 'fail',
-            msg: '账号已存在'
-        });
-    }
-    const result = await UserModel.create({
-        username,
-        password: md5(password),
-        createdAt,
-        updateAt
+  }
+  const isExist = await UserModel.findOne({
+    username
+  });
+  if (isExist) {
+    return (ctx.body = {
+      status: 'fail',
+      msg: '账号已存在'
     });
-    if (!result) {
-        console.log('注册失败');
-        return (ctx.body = {
-            status: 'fail',
-            msg: '注册失败'
-        });
-    } else {
-        console.log('注册成功');
-        return (ctx.body = {
-            status: 'success',
-            msg: '注册成功'
-        });
-    }
+  }
+  const result = await UserModel.create({
+    username,
+    password: md5(password),
+    createdAt,
+    updateAt
+  });
+  if (!result) {
+    console.log('注册失败');
+    return (ctx.body = {
+      status: 'fail',
+      msg: '注册失败'
+    });
+  } else {
+    console.log('注册成功');
+    return (ctx.body = {
+      status: 'success',
+      msg: '注册成功'
+    });
+  }
 };
 // 登录
 const signin = async ctx => {
-    const {
-        username,
-        password
-    } = JSON.parse(ctx.request.body);
-    if (!username || !password) {
-        return (ctx.body = {
-            status: 'fail',
-            msg: '请输入账号和密码'
-        });
+  const { username, password } = JSON.parse(ctx.request.body);
+  if (!username || !password) {
+    return (ctx.body = {
+      status: 'fail',
+      msg: '请输入账号和密码'
+    });
+  }
+  const data = await UserModel.findOne(
+    {
+      username,
+      password: md5(password)
+    },
+    {
+      password: 0
     }
-    const data = await UserModel.findOne({
-        username,
-        password: md5(password)
-    }, {
-        password: 0
+  );
+  if (!data)
+    return (ctx.body = {
+      status: 'fail',
+      msg: '用户名或密码错误'
     });
-    if (!data)
-        return (ctx.body = {
-            status: 'fail',
-            msg: '用户名或密码错误'
-        });
-    ctx.session.user = data;
-    console.log('-----------------');
-    console.log(ctx.session);
-    const id = data._id;
-    const max_age = 1000 * 60 * 10;
+  ctx.session.user = data;
+  console.log('-----------------');
+  console.log(ctx.session);
+  const id = data._id;
+  const max_age = 1000 * 60 * 10;
 
-    ctx.cookies.set('userid', id, {
-        maxAge: max_age,
-        httpOnly: false
-    });
-    ctx.cookies.set('username', username, {
-        maxAge: max_age,
-        httpOnly: false
-    });
+  ctx.cookies.set('userid', id, {
+    maxAge: max_age,
+    httpOnly: false
+  });
+  ctx.cookies.set('username', username, {
+    maxAge: max_age,
+    httpOnly: false
+  });
 
-    ctx.body = {
-        msg: '登录成功',
-        data
-    };
+  ctx.body = {
+    msg: '登录成功',
+    data
+  };
 };
 // 退出
 const signout = async ctx => {
-    console.log(ctx.cookies.get('userid'));
+  console.log(ctx.cookies.get('userid'));
 
-    ctx.session.user = null;
-    ctx.cookies.set('userid', null, {
-        maxAge: 0
-    });
-    ctx.cookies.set('username', null, {
-        maxAge: 0
-    });
-    return (ctx.body = {
-        status: 'success',
-        msg: '退出成功'
-    });
+  ctx.session.user = null;
+  ctx.cookies.set('userid', null, {
+    maxAge: 0
+  });
+  ctx.cookies.set('username', null, {
+    maxAge: 0
+  });
+  return (ctx.body = {
+    status: 'success',
+    msg: '退出成功'
+  });
 };
 
 const createArticle = async ctx => {
-    const user = ctx.session.user;
+  const user = ctx.session.user;
 
-    if (!user)
-        return (ctx.body = {
-            status: 'fail',
-            msg: '请先进行登陆'
-        });
-
-    const {
-        _id
-    } = user;
-    const data = JSON.parse(ctx.request.body);
-    console.log(data);
-    if (!data)
-        return (ctx.body = {
-            status: 'fail',
-            msg: '数据发送失败'
-        });
-
-    const isExist = await ArticleModel.findOne({
-        title: data.title
+  if (!user)
+    return (ctx.body = {
+      status: 'fail',
+      msg: '请先进行登陆'
     });
-    if (isExist)
-        return (ctx.body = {
-            status: 'fail',
-            msg: '文章标题重复'
-        });
-    data.author = _id;
-    const result = await ArticleModel.create(data);
-    if (!result) return ctx.body = {
-        status: 'fail',
-        msg: '文章创建失败'
-    };
 
-    return ctx.body = {
-        status: 'success',
-        msg: '文章创建成功',
-        data: data
-    }
+  const { _id } = user;
+  const data = JSON.parse(ctx.request.body);
+//   console.log(data);
+  if (!data)
+    return (ctx.body = {
+      status: 'fail',
+      msg: '数据发送失败'
+    });
+
+  const isExist = await ArticleModel.findOne({
+    title: data.title
+  });
+  if (isExist)
+    return (ctx.body = {
+      status: 'fail',
+      msg: '文章标题重复'
+    });
+  data.author = _id;
+  const result = await ArticleModel.create(data);
+  if (!result)
+    return (ctx.body = {
+      status: 'fail',
+      msg: '文章创建失败'
+    });
+
+  return (ctx.body = {
+    status: 'success',
+    msg: '文章创建成功',
+    data: data
+  });
+};
+
+const getArticleList = async ctx => {
+  let { page, pagesize } = ctx.query;
+  if (!page) page = 1;
+  if (!pagesize) pagesize = 10;
+  const skip = (Number(page) - 1) * Number(pagesize);
+  const total = await ArticleModel.count();
+
+  const articleList = await ArticleModel.find()
+    .skip(Number(skip))
+    .limit(Number(pagesize));
+
+  return (ctx.body = {
+    status: 'success',
+    data: { total, articleList }
+  });
 };
 
 const getArticleDetail = async ctx => {
-    const {
-        id
-    } = ctx.query;
+  const { id } = ctx.query;
 
-    const data = await ArticleModel
-        .findById(id)
-        .populate('author', {
-            password: 0
-        });
-    if (!data) return ctx.body = {
-        status: 'fail',
-        msg: '获取失败'
-    }
+  const data = await ArticleModel.findById(id).populate('author', {
+    password: 0
+  });
+  if (!data)
+    return (ctx.body = {
+      status: 'fail',
+      msg: '获取失败'
+    });
 
-    const review = data.review + 1;
-}
+  const review = data.review + 1;
+  const updateview = await ArticleModel.findByIdAndUpdate(data.id, {
+    $set: { review }
+  }); // $set 用来指明更新的字段
+
+  return (ctx.body = {
+    status: 'success',
+    data: data
+  });
+};
 
 /* -----------------一些中间件------------------ */
 app.use(session(CONFIG, app));
@@ -255,21 +279,22 @@ app.use(bodyParser());
 // cors 解决跨域
 // ctx.set('Access-Control-Allow-Origin', 'http://www.xlss.link');
 router.all('/api/*', async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000');
-    ctx.set('Access-Control-Allow-Credentials', 'true');
-    await next();
+  ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
+  await next();
 });
 
 router
-    .post('/api/register', register)
-    .post('/api/signin', signin)
-    .get('/api/signout', signout)
+  .post('/api/register', register)
+  .post('/api/signin', signin)
+  .get('/api/signout', signout)
 
-    .post('/api/create', createArticle)
+  .post('/api/create', createArticle)
+  .get('/api/articleList', getArticleList);
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(8080, () => {
-    console.log('listening to port 8080...');
+  console.log('listening to port 8080...');
 });
 // 引入并使用koa-bodyparser中间件 然后就可以直接在ctx.request.body 中获取到JSON格式的POST数据了。
